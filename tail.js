d3aaf08a1a6f1b8132d717e9a69fc81b6e9c7769
MIKE LE MINH NGUYEN <minhnl@yahoo.com>
2022-01-10 15:18:30 -0500

    Create tail.js and a function to return array tail

---
+++ tail.js
@@ -0,0 +1,29 @@
+// FUNCTION IMPLEMENTATION
+const tail = function(array) {
+  return array.slice(1);
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✔️✔️✔️ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Test Case: Check the original array not being modified
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
+//Test Case: check the returned array value
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+// Test Case: Check if one element array would yield an empty array
+assertEqual(tail(["Yo Yo"]).length, 0);
+
+// Test Case: Check if an empty array would yield an empty array
+assertEqual(tail([]).length, 0);
