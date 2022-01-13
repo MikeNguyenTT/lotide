ecda71d83e5d60f66e837e4f66a755bacb917caa
MIKE LE MINH NGUYEN <minhnl@yahoo.com>
2022-01-13 14:54:36 -0500

    implement findKey.js callback function

---
+++ findKey.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✔️✔️✔️ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const findKey = function(obj, callback) {
+  for (const property in obj) {
+    if (callback(obj[property])) {
+      return property;
+    }
+  }
+}
+
+const key = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+
+// TEST CODE
+assertEqual(key, "noma");
