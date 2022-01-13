0e0c0665abb2d2fd00236fd6b0604304afcaa5aa
MIKE LE MINH NGUYEN <minhnl@yahoo.com>
2022-01-13 14:23:40 -0500

    implement map.js callback function

---
+++ map.js
@@ -0,0 +1,42 @@
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✔️✔️✔️ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  // return false if 2 arrays have different length
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  
+  // verify every element in 2 arrays have same both value and type using triple equal operator
+  for (let i = 0; i <= array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const results1 = map(words, word => word[0]);
+assertArraysEqual(results1, ["g", "c", "t", "m", "t"]); // => should PASS
+
+const results2 = map(words, word => word.length);
+assertArraysEqual(results2, [6, 7, 2, 5, 3]); // => should PASS
+
+const results3 = map([1, 3, 6, 7, 9, 34], num => num * 2);
+assertArraysEqual(results3, [2, 6, 12, 14, 18, 68]); // => should PASS
