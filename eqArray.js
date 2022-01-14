7847dbda9b3e881afd0b63ccb55672290eaac780
MIKE LE MINH NGUYEN <minhnl@yahoo.com>
2022-01-14 17:38:05 -0500

    improve eqArray to use recursion to compare multi level nested arrays

--- eqArray.js
+++ eqArray.js
@@ -14,6 +14,9 @@ const eqArrays = function(array1, array2) {
   
   // verify every element in 2 arrays have same both value and type using triple equal operator
   for (let i = 0; i <= array1.length; i++) {
+    if (Array.isArray(array1[i])) {
+      return eqArrays(array1[i], array2[i]);
+    }
     if (array1[i] !== array2[i]) {
       return false;
     }
@@ -27,3 +30,7 @@ assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => should FAIL
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => should FAIL
+
+assertEqual(eqArrays([[2, [3, [7]]], [4]], [[2, [3, [7]]], [4]]), true); 
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
