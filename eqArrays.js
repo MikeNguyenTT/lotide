5779348c01bc58c628698fe9d2b158926054a4ec
MIKE LE MINH NGUYEN <minhnl@yahoo.com>
2022-01-17 13:40:32 -0500

    refactor eqArrays.js to import required dependicies

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✔️✔️✔️ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(array1, array2) {
   // return false if 2 arrays have different length
